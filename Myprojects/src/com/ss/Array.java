2ebe00dad2bd53ef2b2b05386ec03eed44eb1e20
p suneel kumar <paluru.suneelkumar@gmail.com>
2019-04-14 18:33:18 +0530

    Added for development

--- Myprojects/src/com/ss/Array.java
+++ Myprojects/src/com/ss/Array.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Array {
 
 	public static void main(String[] args) {
-		Scanner scn=new Scanner(System.in);
+		/**Scanner scn=new Scanner(System.in);
 			int sum=0;
 		for(int i=0;i<=36;i++)
 		{
@@ -13,7 +13,7 @@ public class Array {
 				sum+=i;
 		}
 		System.out.println(sum);
-
+		**/
 	}
 
 }
